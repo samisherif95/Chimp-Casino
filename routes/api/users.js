35648d3b92a268c0a3e7f3a13fb6a7bfbc194bb4
Sebastian Mendo Lopez <mendo@Sebastians-MacBook-Pro.local>
2020-01-18 18:52:51 -0800

    Fix user auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,9 +4,9 @@ const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
-const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
-const passport = require("passport");
+// const jwt = require("jsonwebtoken");
+// const keys = require("../../config/keys");
+// const passport = require("passport");
 
 
 
@@ -14,66 +14,29 @@ router.get("/test", (req, res) => {
   res.json({ msg: "this is the user route" });
 });
 
-router.get(
-  "/current",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.json({
-      id: req.user.id,
-      username: req.user.username,
-      email: req.user.email
-    });
-  }
-);
-
-
-router.post("/register", (req, res) => {
+router.post('/register', (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
   if (!isValid) {
     return res.status(400).json(errors);
   }
 
-  User.findOne({ username: req.body.username }).then(user => {
-    if (user) {
-      errors.username = "User already exists";
-      return res.status(400).json(errors);
-    } else {
-      const newUser = new User({
-        username: req.body.username,
-        password: req.body.password
-      });
+  User.findOne({ username: req.body.username })
+    .then(user => {
+      if (user) {
+        // Use the validations to send the error
+        errors.username = 'username already exists';
+        return res.status(400).json(errors);
+      } else {
+        const newUser = new User({
+          username: req.body.username,
+          password: req.body.password
+        })
+      }
+    })
+})
 
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser
-            .save()
-            .then(user => {
-              const payload = { id: user.id, username: user.username };
-
-              jwt.sign(
-                payload,
-                keys.secretOrKey,
-                { expiresIn: 3600 },
-                (err, token) => {
-                  res.json({
-                    success: true,
-                    token: "Bearer " + token
-                  });
-                }
-              );
-            })
-            .catch(err => console.log(err));
-        });
-      });
-    }
-  });
-});
-
-
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
 
   if (!isValid) {
@@ -83,34 +46,26 @@ router.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({ username }).then(user => {
-    if (!user) {
-      errors.username = "This user does not exist";
-      return res.status(400).json(errors);
-    }
-
-    bcrypt.compare(password, user.password).then(isMatch => {
-      if (isMatch) {
-        const payload = { id: user.id, username: user.username };
-
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          { expiresIn: 3600 },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token
-            });
-          }
-        );
-      } else {
-        errors.password = "Incorrect password";
-        return res.status(400).json(errors);
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        // Use the validations to send the error
+        errors.username = 'User not found';
+        return res.status(404).json(errors);
       }
-    });
-  });
-});
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({ msg: 'Success' });
+          } else {
+            // And here:
+            errors.password = 'Incorrect password'
+            return res.status(400).json(errors);
+          }
+        })
+    })
+})
 
 module.exports = router;
 
