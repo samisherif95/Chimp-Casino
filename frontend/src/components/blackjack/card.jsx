f1f2c062c9455d7155e5d03c2ba3ad2406d0b79e
Duke <hnguyen1179@berkeley.edu>
2020-01-21 19:57:20 -0800

    blackjack logic

---
+++ frontend/src/components/blackjack/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Card extends React.Component {
+    
+
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        );
+  }
+}
+
+export default Card;
