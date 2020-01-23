17a48528819102e8f67136e373e539f0f2f21526
Sebastian Mendo Lopez <mendo@Sebastians-MacBook-Pro.local>
2020-01-23 15:03:19 -0800

    Chat server

---
+++ frontend/src/components/chat/chat.jsx
@@ -0,0 +1,51 @@
+ import React from 'react';
+const io = require('socket.io-client');
+
+
+
+class Chat extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: []
+        }
+        this.socket = process.env.NODE_ENV === 'production' ? io() : io('http://localhost:8000');
+        this.handleTest = this.handleTest.bind(this)
+    }
+
+    componentDidMount(){ 
+        this.socket.on("receiveMessage", (data) => {
+        this.state.messages.push(data)
+            this.setState(this.state)
+        })
+        
+    }
+
+    handleTest(){
+        this.socket.emit("chat", "kfjsdahfjklsdahklfjshakfhweohdcknc")
+    }
+
+    render() {
+        console.log(this.props.currentUser.username)
+        console.log(this.state.messages)
+        return (
+            <div className='chat-container'>
+                <div className='chat-box'>
+                    
+                </div>
+
+
+
+                {/* <button onClick={this.handleTest}>Click me</button>
+                <ul>
+                    {
+                        this.state.messages.map( (message, idx) => <li key={idx} >{message}</li> )
+                    }
+                </ul> */}
+            </div>
+        )
+    }
+
+}
+
+export default Chat
