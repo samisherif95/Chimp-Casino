3b9980755bbd870ab283b76f5ea6626ff78d5d4a
Evans Shao <evanzshao@gmail.com>
2020-01-27 00:13:05 -0800

    heroku

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require('passport');
 const socket = require('socket.io')
+const path = require('path');
 
 //File Imports
 const User = require("./models/User");
@@ -26,6 +27,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 require('./config/passport')(passport);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 //Routes
 app.use("/api/users", users);
