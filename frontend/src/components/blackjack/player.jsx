b149e93b2b617b97733e448e142bddaa08f85e4b
Duke <hnguyen1179@berkeley.edu>
2020-01-26 18:17:34 -0800

    working on styling, full functionality not done yet

--- frontend/src/components/blackjack/player.jsx
+++ frontend/src/components/blackjack/player.jsx
@@ -1,128 +1,121 @@
-import React from 'react';
-import './blackjack.css';
+// import React from 'react';
+// import './blackjack.css';
 
-class Player extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleHit = this.handleHit.bind(this);
-        this.handleStand = this.handleStand.bind(this);
-        this.handleSplit = this.handleSplit.bind(this);
-        this.handleDouble = this.handleDouble.bind(this);
-    }
+// class Player extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.handleHit = this.handleHit.bind(this);
+//         this.handleStand = this.handleStand.bind(this);
+//         this.handleSplit = this.handleSplit.bind(this);
+//         this.handleDouble = this.handleDouble.bind(this);
+//     }
 
-    handleBet() {
-        // this.state.player.bet()
-        this.props.updateBlackjack();
-    }
+//     handleBet() {
+//         // this.state.player.bet()
+//         this.props.updateBlackjack();
+//     }
 
-    handleHit() {
-        if (this.props.player.bust === false) {
-            this.props.player.hit()
-            if (this.props.player.bust && this.props.player.handSplit.length == 0) {
-                this.nullAllOptions();
-            }
-            if (this.props.player.bust) {
-                document.getElementsByClassName("blackjack-hand")[0].style.color = 'red';
-            }
-            this.props.updateBlackjack();
-        } else if (this.props.player.bustSplit === false) {
-            this.props.player.hitSplit()
-            if (this.props.player.bustSplit) {
-                document.getElementsByClassName("blackjack-handSplit")[0].style.color = 'red';
-                this.nullAllOptions();
-            }
-            this.props.updateBlackjack();
-        }
-    }   
+//     handleHit() {
+//         if (this.props.player.bust === false) {
+//             this.props.player.hit()
+//             if (this.props.player.bust && this.props.player.handSplit.length == 0) {
+//                 this.nullAllOptions();
+//             }
+//             if (this.props.player.bust) {
+//                 document.getElementsByClassName("blackjack-hand")[0].style.color = 'red';
+//             }
+//             this.props.updateBlackjack();
+//         } else if (this.props.player.bustSplit === false) {
+//             this.props.player.hitSplit()
+//             if (this.props.player.bustSplit) {
+//                 document.getElementsByClassName("blackjack-handSplit")[0].style.color = 'red';
+//                 this.nullAllOptions();
+//             }
+//             this.props.updateBlackjack();
+//         }
 
-    handleStand() {
-        this.props.player.stand();
-        this.nullAllOptions();
-        this.props.updateBlackjack();
-    }
+//         if (this.props.checkBust()) {
+//             if (this.props.checkAllBust()) {
+//                 this.newRound();
+//             }
+//         }
+//         this.props.updateBlackjack();
+//     }   
 
-    handleSplit() {
-        this.props.player.splitHand();
-        if (this.props.player.split) {
-            this.props.updateBlackjack();
-        }
-    }
+//     handleStand() {
+//         this.props.player.stand();
+//         this.nullAllOptions();
 
-    handleDouble() {
+//         // Renders that logic into the front end 
+//         this.props.checkStand();
+//         // This updates the state and rerenders the blackjack component with the current phase still being 
+//         // the options phase. It doesn't know it's last in the cycle 
+//         this.props.updateBlackjack();
+//     }
 
-    }
+//     handleSplit() {
+//         this.props.player.splitHand();
+//         if (this.props.player.split) {
+//             this.props.updateBlackjack();
+//         }
+//     }
 
-    nullAllOptions() {
-        document.getElementsByClassName('blackjack-hit')[0].setAttribute('disabled', true)
-        document.getElementsByClassName('blackjack-stand')[0].setAttribute('disabled', true)
-        document.getElementsByClassName('blackjack-split')[0].setAttribute('disabled', true)
-        document.getElementsByClassName('blackjack-double')[0].setAttribute('disabled', true)
-    }
+//     handleDouble() {
+//         this.props.player.doubleDownHand();
+//         this.props.updateBlackjack();
+//     }
 
-    render() {
-        const { player } = this.props; 
 
-        const hand = (player.hand ? player.hand.map(card => {
-            return <li className="blackjack-hand-cards" key={card}> {card} </li>
-        }) : null)
 
-        const handSplit = (player.split ? player.handSplit.map(card => {
-            return <li className="blackjack-handSplit-cards" key={card}> {card} </li>;
-        }) : null)
+//     render() {
+//         const { player } = this.props; 
 
-        const splitPool = ( player.poolSplit > 0 ? <div> Split Balance: {player.poolSplit} </div> : null)
+//         const hand = (player.hand ? player.hand.map(card => {
+//             return <li className="blackjack-hand-cards" key={card}> {card} </li>
+//         }) : null)
 
-        return (
-          <div>
-            <div> Player Container </div>
+//         const handSplit = (player.split ? player.handSplit.map(card => {
+//             return <li className="blackjack-handSplit-cards" key={card}> {card} </li>;
+//         }) : null)
 
-            <section className="blackjack-player-info">
-                <div className="blackjack-player-id">
-                    Player: {player.userId}
+//         const splitPool = ( player.poolSplit > 0 ? <div> Split Balance: {player.poolSplit} </div> : null)
 
-                </div>
-                <div className="blackjack-player-poolSplit">
-                    {splitPool}
-                </div>
-                <div className="blackjack-player-balance">
-                    Balance: {player.balance}
-                </div>
-            </section>
+//         return (
+//           <div>
+//             <div> Player Container </div>
 
-            <div className="">
-                Hand:
-                <ul className="blackjack-hand">
-                    {hand}
-                </ul>
+//             <section className="blackjack-player-info">
+//                 <div className="blackjack-player-id">
+//                     Player: {player.userId}
+
+//                 </div>
+//                 <div className="blackjack-player-poolSplit">
+//                     {splitPool}
+//                 </div>
+//                 <div className="blackjack-player-balance">
+//                     Balance: {player.balance}
+//                 </div>
+//             </section>
+
+//             <div className="">
+//                 Hand:
+//                 <ul className="blackjack-hand">
+//                     {hand}
+//                 </ul>
                 
-                {player.split ? "Split Hand" : null}
-                <ul className="blackjack-handSplit">
-                    {handSplit}
-                </ul>
-            </div>
+//                 {player.split ? "Split Hand" : null}
+//                 <ul className="blackjack-handSplit">
+//                     {handSplit}
+//                 </ul>
+//             </div>
 
-            <div className="blackjack-hand-values">
-                Hand value: {player.getHandValue(player.hand)}
-                {player.split ? <div>Split Hand value: {player.getHandValue(player.handSplit)} </div> : null}
-            </div>
+//             <div className="blackjack-hand-values">
+//                 Hand value: {player.getHandValue(player.hand)}
+//                 {player.split ? <div>Split Hand value: {player.getHandValue(player.handSplit)} </div> : null}
+//             </div>
+//           </div>
+//         );
+//     }
+// }
 
-            <section className="blackjack-options">
-                <button className="blackjack-hit" onClick={this.handleHit}>
-                    Hit!
-                </button>
-                <button className="blackjack-stand" onClick={this.handleStand}>
-                    Stand!
-                </button>
-                <button className="blackjack-split" onClick={this.handleSplit}>
-                    Split hand!
-                </button>
-                <button className="blackjack-double" onClick={this.handleDouble}>
-                    Double down!
-                </button>
-            </section>
-          </div>
-        );
-    }
-}
-
-export default Player;
+// export default Player;
