075ece1557dd6e175069ee6b0a191c1a72acd084
Evans Shao <evanzshao@gmail.com>
2020-02-03 10:37:02 -0800

    poker changes

--- frontend/src/components/blackjack/blackjack.jsx
+++ frontend/src/components/blackjack/blackjack.jsx
@@ -9,6 +9,7 @@ import imageHash from "./blackjackImages";
 class Blackjack extends React.Component {
     constructor(props) {
         super(props)
+        console.log(this.props);
         const blackjack = new GameLogic.Blackjack();
         this.state = {
             blackjack: blackjack,
