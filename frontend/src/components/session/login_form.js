97d7c483bf5034c54c33ae0504ab68b18526bd58
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2020-02-15 23:14:45 -0800

    demo user implementation

--- frontend/src/components/session/login_form.js
+++ frontend/src/components/session/login_form.js
@@ -14,6 +14,7 @@ class LoginForm extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.renderErrors = this.renderErrors.bind(this);
         this.handleCancel = this.handleCancel.bind(this);
+        this.handleDemoUser = this.handleDemoUser.bind(this);
     }
 
     componentWillReceiveProps(nextProps) {
@@ -84,6 +85,43 @@ class LoginForm extends React.Component {
         this.props.closeModal();
     }
 
+    handleDemoUser(e){
+        e.preventDefault();
+        
+        let demoUsername = ['d','e', 'm', 'o']
+        let demoPassword = ['p', 'a', 's', 's', 'w', 'o', 'r', 'd']
+        const usernameInterval = setInterval(() => {
+            const first = demoUsername.splice(0, 1);
+            this.setState(
+                { username: this.state.username + first[0] },
+                () => {
+                    if (!demoUsername.length) {
+                        clearInterval(usernameInterval);
+                        const passwordInterval = setInterval(() => {
+                            const first = demoPassword.splice(0, 1);
+                            this.setState(
+                                { password: this.state.password + first[0] },
+                                () => {
+                                    if (!demoPassword.length) {
+                                        clearInterval(passwordInterval)
+                                        this.props.login(this.state)
+                                            .then(() => {
+                                                if (!Object.keys(this.state.errors).length) {
+                                                    this.props.history.push('/lobbies');
+                                                    this.props.closeModal();
+                                                }
+                                            })
+                                    }
+
+                                }
+                            )
+                        }, 50)
+                    }
+                }
+            );
+        }, 50);
+    }
+
     render() {
         return (
             <div>
@@ -95,24 +133,26 @@ class LoginForm extends React.Component {
                     <p>Please Log In</p>
                 </div>
                 <form className='form-login' onSubmit={this.handleSubmit}>
-                        <input type="text"
-                            id='username'
-                            value={this.state.username}
-                            onChange={this.update('username')}
-                            placeholder="Username"
-                            className='input'
-                        />
-                        {Object.keys(this.state.errors).length > 0 ? this.renderErrors()[0] : null}
-                        <input type="password"
-                            id='password'
-                            value={this.state.password}
-                            onChange={this.update('password')}
-                            placeholder="Password"
-                            className='input'
-                        />
-                        {Object.keys(this.state.errors).length > 0 ? this.renderErrors()[1] : null}
-                        <input id='submit-login' type="submit" value="Log In" />
+                    <button id='demo-user' type='submit' onClick={this.handleDemoUser}>Demo User</button>
+                    <input type="text"
+                        id='username'
+                        value={this.state.username}
+                        onChange={this.update('username')}
+                        placeholder="Username"
+                        className='input'
+                    />
+                    {Object.keys(this.state.errors).length > 0 ? this.renderErrors()[0] : null}
+                    <input type="password"
+                        id='password'
+                        value={this.state.password}
+                        onChange={this.update('password')}
+                        placeholder="Password"
+                        className='input'
+                    />
+                    {Object.keys(this.state.errors).length > 0 ? this.renderErrors()[1] : null}
+                    <input id='submit-login' type="submit" value="Log In" />
                 </form>
+
                 <div className="text-center">
                     <p className="">Don't have an account?
                         <a onClick={this.props.openSignUp}>Sign Up</a>
